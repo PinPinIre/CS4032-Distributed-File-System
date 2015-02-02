f5283b2f9c0bdc8cf1fc8030aacb033c43d167ee
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-02 16:47:16 +0000

    Update start script

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+import sqlite3 as db
+
+DATABASE = "Database/directories.db"
+con = db.connect(DATABASE)
+with con:
+    cur = con.cursor()
+    cur.execute('INSERT INTO Servers (Server, Port) VALUES ("0.0.0.0", 8006)')
+    cur.execute('INSERT INTO Servers (Server, Port) VALUES ("0.0.0.0", 8009)')
+    cur.execute('INSERT INTO Servers (Server, Port) VALUES ("0.0.0.0", 8010)')
+    con.commit()
+    con.close()
--- start.sh
+++ start.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-python server.py $1
+
+mkdir -p ClientFiles
+mkdir -p Database
+mkdir -p DirectoryServerFiles/8006
+mkdir -p DirectoryServerFiles/8009
+mkdir -p DirectoryServerFiles/8010
+
+python src/Servers/directoryServer.py 8005 &
+python src/Servers/lockServer.py 8007 &
+python src/Servers/fileServer.py 8006 &
+python src/Servers/fileServer.py 8009 &
+python src/Servers/fileServer.py 8010 &
+
+python src/setup.py
