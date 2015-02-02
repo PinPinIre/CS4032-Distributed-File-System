b72cbf0aa2ceed0d9005f7ef6f7b5bb44d94e079
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-02 17:10:55 +0000

    Fix db close

--- src/setup.py
+++ src/setup.py
@@ -8,4 +8,4 @@ with con:
     cur.execute('INSERT INTO Servers (Server, Port) VALUES ("0.0.0.0", 8009)')
     cur.execute('INSERT INTO Servers (Server, Port) VALUES ("0.0.0.0", 8010)')
     con.commit()
-    con.close()
+    cur.close()
